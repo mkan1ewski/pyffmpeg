4969c0784be133f178f256e85461cf8854b6bb94
mkan1ewski <maciek1717.mk@gmail.com>
2025-10-25 18:37:59 +0200

    docs: added type hints

--- src/pyffmpeg/__init__.py
+++ src/pyffmpeg/__init__.py
@@ -1,8 +1,12 @@
 from pyffmpeg.utils import input, merge_outputs, get_args
+from pyffmpeg.node import Stream, Node
+from collections.abc import Callable
+from typing import Any
 
 
-def __getattr__(name):
-    def wrapper(obj, *args, **kwargs):
+def __getattr__(name: str) -> Callable[..., Any]:
+    def wrapper(obj: Stream | Node, *args, **kwargs) -> Any:
         return getattr(obj, name)(*args, **kwargs)
 
     return wrapper
+
