186b8fc54a277ddbcf79625dca61736b012649bd
mkan1ewski <maciek1717.mk@gmail.com>
2025-11-23 18:47:02 +0100

    tests: parsing description

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -23,3 +23,16 @@ def test_overlay_filter_name():
 def test_scale_filter_name():
     data = get_parsed_filter_data("scale")
     assert data.get("filter_name") == "scale"
+
+
+def test_overlay_description():
+    data = get_parsed_filter_data("overlay")
+    assert data.get("description") == "Overlay a video source on top of the input."
+
+
+def test_scale_description():
+    data = get_parsed_filter_data("scale")
+    assert (
+        data.get("description")
+        == "Scale the input video size and/or convert the image format."
+    )
