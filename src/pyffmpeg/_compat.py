c1ae8e8dfe258cef2e1109d375893475281cf541
mkan1ewski <maciek1717.mk@gmail.com>
2025-11-27 23:24:54 +0100

    refactor: renamed StreamWrapper to StreamCompatWrapper

--- src/pyffmpeg/_compat.py
+++ src/pyffmpeg/_compat.py
@@ -25,12 +25,12 @@ def wrap_stream_output(func):
     def wrapper(*args, **kwargs):
         return_object = func(*args, **kwargs)
         if isinstance(return_object, Stream):
-            return StreamWrapper(return_object)
+            return StreamCompatWrapper(return_object)
         if isinstance(return_object, Sequence) and all(
             isinstance(stream, (Stream | TypedStream | IndexedStream))
             for stream in return_object
         ):
-            return [StreamWrapper(stream) for stream in return_object]
+            return [StreamCompatWrapper(stream) for stream in return_object]
         return return_object
 
     return wrapper
@@ -39,7 +39,9 @@ def wrap_stream_output(func):
 def wrap_stream_input(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        args = [arg.stream if isinstance(arg, StreamWrapper) else arg for arg in args]
+        args = [
+            arg.stream if isinstance(arg, StreamCompatWrapper) else arg for arg in args
+        ]
         return func(*args, **kwargs)
 
     return wrapper
@@ -48,7 +50,7 @@ def wrap_stream_input(func):
 input = wrap_stream_output(input)
 
 
-class StreamWrapper:
+class StreamCompatWrapper:
     def __init__(self, stream: Stream | TypedStream | IndexedStream):
         self.stream: Stream | TypedStream | IndexedStream = stream
 
@@ -59,44 +61,52 @@ class StreamWrapper:
             if callable(attr):
                 return wrap_stream_input(wrap_stream_output(attr))
             if isinstance(attr, (TypedStream, IndexedStream)):
-                return StreamWrapper(attr)
+                return StreamCompatWrapper(attr)
             if isinstance(attr, Sequence) and all(isinstance(x, Stream) for x in attr):
-                return [StreamWrapper(stream) for stream in attr]
+                return [StreamCompatWrapper(stream) for stream in attr]
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, StreamWrapper):
+        if not isinstance(other, StreamCompatWrapper):
             return NotImplemented
         return self.stream == other.stream
 
     def __hash__(self) -> int:
         return hash(self.stream)
 
-    def __getitem__(self, key: str) -> "StreamWrapper":
-        return StreamWrapper(self.stream[key])
+    def __getitem__(self, key: str) -> "StreamCompatWrapper":
+        return StreamCompatWrapper(self.stream[key])
 
-    def filter(self, filter_name: str, *args, **kwargs) -> "StreamWrapper":
+    def filter(self, filter_name: str, *args, **kwargs) -> "StreamCompatWrapper":
         """Custom filter with a single input and a single output"""
-        return StreamWrapper(self.stream._apply_filter(filter_name, args, kwargs)[0])
+        return StreamCompatWrapper(
+            self.stream._apply_filter(filter_name, args, kwargs)[0]
+        )
 
 
 def filter(
-    stream_spec: list[StreamWrapper] | StreamWrapper, filter_name: str, *args, **kwargs
-) -> StreamWrapper:
-    if isinstance(stream_spec, StreamWrapper):
+    stream_spec: list[StreamCompatWrapper] | StreamCompatWrapper,
+    filter_name: str,
+    *args,
+    **kwargs,
+) -> StreamCompatWrapper:
+    if isinstance(stream_spec, StreamCompatWrapper):
         stream_spec = [stream_spec.stream]
-    if all(isinstance(s, StreamWrapper) for s in stream_spec):
+    if all(isinstance(s, StreamCompatWrapper) for s in stream_spec):
         stream_spec = [s.stream for s in stream_spec]
-    return StreamWrapper(
+    return StreamCompatWrapper(
         stream_spec[0]._apply_filter(filter_name, args, kwargs, inputs=stream_spec)[0]
     )
 
 
 def filter_multi_output(
-    stream_spec: list[StreamWrapper] | StreamWrapper, filter_name: str, *args, **kwargs
-) -> StreamWrapper:
-    if isinstance(stream_spec, StreamWrapper):
+    stream_spec: list[StreamCompatWrapper] | StreamCompatWrapper,
+    filter_name: str,
+    *args,
+    **kwargs,
+) -> StreamCompatWrapper:
+    if isinstance(stream_spec, StreamCompatWrapper):
         stream_spec = [stream_spec.stream]
-    if all(isinstance(s, StreamWrapper) for s in stream_spec):
+    if all(isinstance(s, StreamCompatWrapper) for s in stream_spec):
         stream_spec = [s.stream for s in stream_spec]
     node = FilterNode(
         filter_name=filter_name,
