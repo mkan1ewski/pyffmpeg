221504104d3e3c4db321944d836115f462cb859c
mkan1ewski <maciek1717.mk@gmail.com>
2025-11-29 12:33:58 +0100

    chore: removed old c parsing files and related functions

--- src/pyffmpeg/utils.py
+++ src/pyffmpeg/utils.py
@@ -1,5 +1,3 @@
-import re
-import subprocess
 from typing import Sequence
 
 from pyffmpeg.node import (
@@ -12,86 +10,6 @@ from pyffmpeg.node import (
     GraphSorter,
     CommandBuilder,
 )
-
-
-def extract_filenames(makefile: str = "Makefile") -> list[str]:
-    """Extracts filenames defining filters from a Makefile."""
-    filenames = []
-    with open(makefile) as f:
-        for line in f:
-            match = re.search(r"FILTER\)\s*\+=\s*([^\s]+)", line)
-            if match:
-                filenames.append(match.group(1))
-    return filenames
-
-
-def get_c_files_names(object_files: list[str]) -> list[str]:
-    """Changes object file names to equivalent .c file names."""
-    return [file[:-1] + "c" for file in object_files]
-
-
-def preprocess_code(filename) -> str | None:
-    """Preprocesses a C file using clang."""
-    result = subprocess.run(
-        [
-            "clang",
-            "-E",
-            "-D__attribute__(x)=",
-            "-D__extension__=",
-            "-I",
-            "./pycparser/utils/fake_libc_include",
-            "-I",
-            "./FFmpeg",
-            "-I",
-            "./FFmpeg/libavcodec/",
-            f"./FFmpeg/libavfilter/{filename}",
-        ],
-        capture_output=True,
-        text=True,
-    )
-
-    if result.returncode == 0:
-        preprocessed_code = result.stdout
-        return preprocessed_code
-    else:
-        return None
-
-
-def unify_names(all_names):
-    for i, names in enumerate(all_names):  # iterate through outer list
-        for j, name in enumerate(names):  # iterate through inner list
-            # modify the string and assign it back to the same position
-            all_names[i][j] = (
-                name.replace("(", "")
-                .replace(")", "")
-                .replace("/", "_")
-                .replace("|", "")
-            )
-
-
-def unify_keys(all_options):
-    for i, options_list in enumerate(all_options):
-        for j, d in enumerate(options_list):
-            new_dict = {}
-            for key, value in d.items():
-                # Replace hyphen with underscore in the key
-                new_key = key.replace("-", "_").replace(">", "_").replace("+", "_")
-                if new_key in [
-                    "pass",
-                    "and",
-                    "lambda",
-                    "class",
-                    "in",
-                    "or",
-                    "if",
-                    "raise",
-                    "as",
-                ]:
-                    new_key += "_"
-                if new_key[0].isdigit():
-                    new_key = "_" + new_key
-                new_dict[new_key] = value
-            all_options[i][j] = new_dict
 
 
 def input(path: str, **kwargs) -> Stream:
