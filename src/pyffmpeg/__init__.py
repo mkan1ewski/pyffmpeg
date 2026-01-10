082bfeda73d15fb504132c00cb5d78bae49098a6
mkan1ewski <maciek1717.mk@gmail.com>
2026-01-10 17:57:18 +0100

    refactor: __getattr__  simplified

--- src/pyffmpeg/__init__.py
+++ src/pyffmpeg/__init__.py
@@ -12,14 +12,18 @@ import inspect
 def __getattr__(name: str) -> Callable[..., Any]:
     def wrapper(obj: Stream | Node | list[Stream], *args, **kwargs) -> Any:
         target_obj = obj
+        inputs_list = []
+
         if isinstance(obj, (list, tuple)) and obj:
             target_obj = obj[0]
             inputs_list = obj[1:]
+
         if not isinstance(target_obj, (Stream, Node)):
             raise TypeError(
-                f"Cannot call '{name}'. First argument "
-                f"is '{type(obj)}', but must be Stream, Node or a list of Streams."
+                f"Cannot call '{name}'. First argument is '{type(obj)}', "
+                f"but must be Stream, Node or a list of Streams."
             )
+
         try:
             method = getattr(target_obj, name)
         except AttributeError:
@@ -27,11 +31,11 @@ def __getattr__(name: str) -> Callable[..., Any]:
                 f"Module 'pyffmpeg' does not have a function '{name}' "
                 f"and an object of type '{type(target_obj)}' does not have a method with that name."
             )
+
         sig = inspect.signature(method)
         if "inputs" in sig.parameters:
-            args = (inputs_list, *args)
-            return method(*args, **kwargs)
-        else:
-            return method(*args, **kwargs)
+            return method(inputs_list, *args, **kwargs)
+
+        return method(*args, **kwargs)
 
     return wrapper
