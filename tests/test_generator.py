df28a2bba50eae30aca76a4c73e3ebe9bbd156bd
mkan1ewski <maciek1717.mk@gmail.com>
2026-01-20 18:49:47 +0100

    test: changed generator test to include None in type hint

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -17,17 +17,20 @@ def test_split_code_streams():
 
 def test_overlay_code_streams():
     method_code = get_generated_method("overlay")
-    assert "    def overlay(self, overlay: \"Stream\"" in method_code
+    assert '    def overlay(self, overlay_stream: "Stream"' in method_code
 
 
 def test_split_code_all_options():
     method_code = get_generated_method("split")
-    assert "    def split(self, outputs: int = 2) -> \"Stream\":" in method_code
+    assert (
+        '    def split(self, outputs: int | None = None) -> "FilterMultiOutput":'
+        in method_code
+    )
 
 
 def test_overlay_code_some_options():
     method_code = get_generated_method("overlay")
     assert (
-        "    def overlay(self, overlay: \"Stream\", x: str = \"0\", y: str = \"0\", eof_action: Literal['repeat', 'endall', 'pass'] | int = \"repeat\", eval: Literal['init', 'frame'] | int = \"frame\", shortest: bool = False"
+        "    def overlay(self, overlay_stream: \"Stream\", x: str | None = None, y: str | None = None, eof_action: Literal['repeat', 'endall', 'pass'] | int | None = None, eval: Literal['init', 'frame'] | int | None = None, shortest: bool | None = None"
         in method_code
     )
